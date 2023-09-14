8c2814236268e6b18c9c2def8a776489206824ec
Petr Veselý <petrvesely@Petr-MacBook-Air.local>
2023-09-14 20:01:12 +0200

    slider sipky

--- src/js/main.js
+++ src/js/main.js
@@ -26,4 +26,24 @@
 
    });
 
+   $('.slider').slick({
+      slidesToShow: 3,
+      prevArrow: $('.arrows__left'),
+      nextArrow: $('.arrows__right'),
+      responsive: [
+         {
+           breakpoint: 1200,
+           settings: {
+             slidesToShow: 2,
+           }
+         },
+         {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 1,
+         }
+      },
+      ]
+   });
+
  });
